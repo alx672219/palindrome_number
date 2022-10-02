e80a812add0b502c569fa64432ab2550fc26086a
Alex <113051612+alx672219@users.noreply.github.com>
2022-10-01 23:44:49 -0400

    Add files via upload

---
+++ palindrome_number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        string = str(x)
+        for i in range(len(string)//2):
+            if string[i] != string[-i - 1]:
+                return False
+        return True
